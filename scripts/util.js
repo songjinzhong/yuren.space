a922020a75e46ea147d4a59f89c9919a2c13c1ce
songjinzhong <648660165@qq.com>
2016-12-19 23:59:51 +0800

    修改标签排列方式

--- scripts/util.js
+++ scripts/util.js
@@ -4,3 +4,13 @@
 hexo.extend.helper.register('ds_key', function(key){
   return key.match(/\/([^\/]+)\/$/)[1];
 });
+hexo.extend.helper.register('sortCategories', function(catMuch){
+  var arr = [];
+  for(var a in catMuch){
+    arr.push(a);
+  }
+  arr = arr.sort(function(a, b){
+    return catMuch[b] - catMuch[a] || b.length - a.length;
+  })
+  return arr;
+});
